f20df83d35f7e01c338feefaf24cc5dd376fc90d
usran123 <usranjoki.@gmail.com>
2025-05-30 14:31:40 +0700

    first commit/Video ke 3 konsep API (JSON)

---
+++ json/latihan1.php
@@ -0,0 +1,32 @@
+<?php
+
+// $mahasiswa = [
+//     [
+//         "nama" => "Usran Bangun Fajar HUtagalung",
+//         "nim" => "2217020150",
+//         "email" => "usranhutagalung04@gmail."
+//     ],
+//     [
+//         "nama" => "Muhammad Nurul Fajri",
+//         "nim" => "2217020054",
+//         "email" => "nurulfajri@gmail.com"
+//     ],
+//     [
+//         "nama" => "M-Irfandi Atarza",
+//         "nim" => "22170200053",
+//         "email" => "afandiatarza@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO(
+    'mysql:host=localhost; dbname=mahasiswa',
+    'root',
+    ''
+);
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+$data = json_encode($mahasiswa);
+echo $data;
+?>
